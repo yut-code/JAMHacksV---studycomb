152ddac7bdb71f5c875a397a65bda0fe80847d09
yut-code <yut3335@gmail.com>
2022-04-22 22:31:56 -0400

    website
    html+css+pictures

---
+++ loginfunc.js
@@ -0,0 +1,24 @@
+var objPeople = [
+  {
+    username:"Clara"
+    password:"pass1"
+  },
+  {
+    username:"Teresa"
+    password:"pass2"
+  },
+  {
+    username:"Omar"
+    password:"pass3"
+  },
+  {
+    username:"Gabe"
+    password:"pass4"
+  }
+]
+
+//login functionality
+function login() {
+//retrieve data input from the from
+var username=document.getElementById()
+}
